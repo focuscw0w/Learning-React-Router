7d6bbc546e9f1fadfdaeb0d0984822005c520d86
unknown <jamrichfilip16@gmail.com>
2024-01-30 17:36:19 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes, Link } from "react-router-dom";
+import Home from "./Home";
+import NotFound from "./NotFound";
+import BookRoutes from "./BookRoutes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/books">Books</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books/*" element={<BookRoutes />} />
+        {/*<Route  path="/books" element={<BookList/>} />
+        <Route  path="/books/:id" element={<Book />} />
+        <Route  path="/books/new" element={<NewBook />} />*/}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/NotFound.js
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const NotFound = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/", {});
+    }, 1000);
+  });
+  return <h1>NotFound</h1>;
+};
+
+export default NotFound;
